edb505d7610443097f814fc21d826553481bcd9d
masaud04 <ahmedmasaud942@gmail.com>
2025-05-08 13:35:29 +0600

    initialize front-end with Vite, React, and tailwindcss v4

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl text-center capitalize font-bold text-blue-100 shadow-2xl bg-slate-500 py-4">
+        this is vite project
+      </h2>
+    </>
+  );
+}
+
+export default App;
